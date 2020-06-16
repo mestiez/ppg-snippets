06eee449d56e335cee31c6a905fb149d5549cb78
zooi <32131519+mestiez@users.noreply.github.com>
2020-06-16 03:29:53 +0200

    Update call method on activation.cs

--- call method on activation.cs
+++ call method on activation.cs
@@ -1,5 +1,8 @@
 //add a built-in component that invokes a delegate when the item receives an activation signal
 Instance.AddComponent<UseEventTrigger>().Action = () =>
 {
+    //everything inside this delegate will be executed
+    
+    //show a notification to the player
     ModAPI.Notify("I'VE BEEN USED!");
 };
