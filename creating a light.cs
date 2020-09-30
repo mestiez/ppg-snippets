f0fb27b68bb03b937bcbd56b8b72b982c97c177e
Mestiez <m.pereira@student.fontys.nl>
2020-09-30 20:56:52 +0200

    1.11b2

---
+++ creating a light.cs
@@ -0,0 +1,14 @@
+//creating a light object with myself as a parent, a red colour, a radius of 5 units, and a brightness of 1 (default)
+var glow = ModAPI.CreateLight(transform, Color.red, 5, 1);
+
+//change the colour to whatever the hell
+glow.Color = Color.yellow;
+
+//blind the player
+glow.Brightness = 1500;
+
+//devour the world
+glow.Radius = 1000;
+
+//its a component of a standalone GameObject so you can set its position
+glow.transform.localPosition = new Vector3(0,0,0);
